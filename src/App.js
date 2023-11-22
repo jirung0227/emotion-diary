0a2f34d98df89218e6efe25577b6b265ec8a41b5
Jung ji hyung <gw03176370@gmail.com>
2023-11-22 21:27:08 +0900

    feat : [7-4] 프로젝트 기초 공사 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import RouteTest from "./components/RouteTest";
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
 import { Diary } from "./pages/Diary";
 import { Edit } from "./pages/Edit";
 import { Home } from "./pages/Home";
 import { New } from "./pages/New";
 
 function App() {
+  const env = process.env;
+  env.PUBLIC_URL = env.PUBLIC_URL || "";
   return (
     <BrowserRouter>
       <div className='App'>
-        <h2>App.js</h2>
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton text={"왼쪽 버튼"} onClick={() => alert("왼쪽 클릭")} />
+          }
+          rightChild={
+            <MyButton
+              text={"오른쪽 버튼"}
+              onClick={() => alert("오른쪽 클릭")}
+            />
+          }
+        />
+        <MyButton text={"버튼"} onClick={() => alert("aa")} type={"positive"} />
+        <MyButton text={"버튼"} onClick={() => alert("aa")} type={"negative"} />
+
+        <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion2.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion3.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion4.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion5.png`} />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/new' element={<New />} />
@@ -19,7 +41,6 @@ function App() {
           {/* 2. Query String - ?id=10&mode=dark */}
           <Route path='/edit' element={<Edit />} />
         </Routes>
-        <RouteTest />
       </div>
     </BrowserRouter>
   );
